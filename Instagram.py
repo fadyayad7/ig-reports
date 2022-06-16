069ddfe3210c13e4c8a884f4f30daf122c06bea6
fadyayad7 <fady.ayad7@gmail.com>
2022-06-16 20:02:23 +0200

    changed readme

--- Instagram.py
+++ Instagram.py
@@ -34,7 +34,6 @@ class Instagram:
         following = [followee.username for followee in self.profile.get_followees()]
         notFollowingBack = [nfb for nfb in following if nfb not in followers]
 
-        print(notFollowingBack)
         for nfb in notFollowingBack:
             with open("not_following_back.txt","a+") as f:
                 f.write(nfb+'\n')
