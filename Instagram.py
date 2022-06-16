4cb35e034894820f60ac78f3f7ec48fa2c4a9eb3
fadyayad7 <fady.ayad7@gmail.com>
2022-06-16 19:59:04 +0200

    added 3 reports ✅

---
+++ Instagram.py
@@ -0,0 +1,50 @@
+import instaloader
+
+class Instagram:
+
+    def __init__(self, username):
+        self.username = username
+        self.loader = instaloader.Instaloader()
+        self.profile = instaloader.Profile.from_username(self.loader.context,self.username)
+        open('followers.txt', 'w').close()
+        open('following.txt', 'w').close()
+        open('not_following_back.txt', 'w').close()
+
+
+    def login(self):
+        return self.loader.load_session_from_file(self.username)
+
+    def getFollowers(self):
+        for followers in self.profile.get_followers():
+            with open("followers.txt","a+") as f:
+                f.write(followers.username+'\n')
+        
+        print("Writing followers...")
+
+
+    def getFollowing(self):
+        for followees in self.profile.get_followees():
+            with open("following.txt","a+") as f:
+                f.write(followees.username+'\n')
+
+        print("Writing followees...")
+
+    def getNotFollowingBack(self):
+        followers = [follower.username for follower in self.profile.get_followers()]
+        following = [followee.username for followee in self.profile.get_followees()]
+        notFollowingBack = [nfb for nfb in following if nfb not in followers]
+
+        print(notFollowingBack)
+        for nfb in notFollowingBack:
+            with open("not_following_back.txt","a+") as f:
+                f.write(nfb+'\n')
+
+        print("Writing not following back...")
+            
+
+        
+        
+        
+
+
+
---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import OrderedDict
+from dotenv import dotenv_values
+from Instagram import Instagram
+
+username = dotenv_values(".env").get('username')
+
+instagram = Instagram(username)
+instagram.login()
+
+instagram.getFollowers()
+instagram.getFollowing()
+instagram.getNotFollowingBack()
+
